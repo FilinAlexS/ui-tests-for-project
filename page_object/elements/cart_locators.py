742699b53f0aef7fb8c32e837724f469a57b36b0
Aleksandr Filin <xz@fandm.com>
2023-12-24 17:19:40 +0300

    Project improvement

--- page_object/elements/cart_locators.py
+++ page_object/elements/cart_locators.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 
 class CartLocators:
     BUTTON_CART_ADD = (By.XPATH, "//*[contains(@class, 'button-group')]/button[1]")
-    CART_BUTTON_TOP = (By.XPATH, "//*[@id='cart']//span")
+    CART_BUTTON_TOP = (By.XPATH, "//*[@id='cart']//span/..")
     LINK_TO_CART = (By.XPATH, "//*/a[.=' View Cart']")
     QUANTITY_PRODUCT_IN_CART = (By.XPATH, "//*[@id='content']/form/div/table/tbody/tr")
     REMOVE_PRODUCT = (By.XPATH, "//*/table//button[2]")
